6dc80a14edf12b3b1e6e41f615536ef65509d64c
shreyaabh <41884465+shreyaabh@users.noreply.github.com>
2021-07-15 11:24:29 +0545

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import question,askUser,answer,askLawyer
+from .models import question,askUser,answer,askLawyer,Points
 
 # Register your models here.
 admin.site.register(askUser)
 admin.site.register(question)
 admin.site.register(askLawyer)
 admin.site.register(answer)
+admin.site.register(Points)
