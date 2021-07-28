9dc21f9123c1d5db22129a6e9616ebdf29f2b2f9
unknown <shrbhandari2000@gmail.com>
2021-07-28 09:40:39 +0545

    Updated

--- wakil/views.py
+++ wakil/views.py
@@ -9,20 +9,20 @@ class profileModelViewset(viewsets.ModelViewSet):
     serializer_class=ProfileSerializer
 
 class educationModelViewset(viewsets.ModelViewSet):
-    queryset=Profile.objects.all()
-    serializer_class=ProfileSerializer
+    queryset=Education.objects.all()
+    serializer_class=EducationSerializer
 
 class experienceModelViewset(viewsets.ModelViewSet):
-    queryset=Profile.objects.all()
-    serializer_class=ProfileSerializer
+    queryset=Experience.objects.all()
+    serializer_class=ExperienceSerializer
 
 class awardsModelViewset(viewsets.ModelViewSet):
-    queryset=Profile.objects.all()
-    serializer_class=ProfileSerializer
+    queryset=Awards.objects.all()
+    serializer_class=AwardsSerializer
 
 class linkModelViewset(viewsets.ModelViewSet):
-    queryset=Profile.objects.all()
-    serializer_class=ProfileSerializer
+    queryset=Link.objects.all()
+    serializer_class=LinkSerializer
 
     
 
